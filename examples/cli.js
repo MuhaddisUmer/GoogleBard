de6de7e3104ce5d8bf2fa2c68c9e4c202a03ce40
muhaddisumer <muhaddis.umer003@gmail.com>
2023-07-17 15:49:09 +0500

    Response added

--- examples/cli.js
+++ examples/cli.js
@@ -1,8 +1,12 @@
 import express from "express";
+import bodyParser from 'body-parser';
+
 import { Bard } from "../dist/index.js";
 
 const server = express();
-server.use(express.json());
+// server.use(express.json());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 
 let cookies = `NID=; SID=; __Secure-1PSID=YwgB5fzSJXty5aAWAVvaQbmB61eLZ-FJqScw7rzdIdxsSyKLyn0305On6IboG12DHsDhew.; __Secure-3PSID=; HSID=; SSID=; APISID=; SAPISID=; __Secure-1PAPISID=; __Secure-3PAPISID=; SIDCC=; __Secure-1PSIDCC=; __Secure-3PSIDCC=`;
 let bot = new Bard(cookies, {
@@ -21,6 +25,7 @@ server.post("/ask", async (req, res) => {
   try {
     const { prompt, conversationId } = req['body'];
     let response = await bot.ask(prompt, conversationId);
+    console.log('response = ', response);
     res.status(200).send(response);
   } catch(e) {res.status(400).send({})}
 });
