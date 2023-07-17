cb684f6f439e3fc740753ae337c7e25280e8ce70
muhaddisumer <muhaddis.umer003@gmail.com>
2023-07-17 12:59:33 +0500

    Made Proxy Server

--- examples/cli.js
+++ examples/cli.js
@@ -1,42 +1,30 @@
+import express from "express";
 import { Bard } from "../dist/index.js";
-import { config } from "dotenv";
-import readline from "readline";
 
-config();
+const server = express();
+server.use(express.json());
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let cookies = `NID=; SID=; __Secure-1PSID=; __Secure-3PSID=; HSID=; SSID=; APISID=; SAPISID=; __Secure-1PAPISID=; __Secure-3PAPISID=; SIDCC=; __Secure-1PSIDCC=; __Secure-3PSIDCC=`;
-
+let cookies = `NID=; SID=; __Secure-1PSID=YwgB5fzSJXty5aAWAVvaQbmB61eLZ-FJqScw7rzdIdxsSyKLyn0305On6IboG12DHsDhew.; __Secure-3PSID=; HSID=; SSID=; APISID=; SAPISID=; __Secure-1PAPISID=; __Secure-3PAPISID=; SIDCC=; __Secure-1PSIDCC=; __Secure-3PSIDCC=`;
 let bot = new Bard(cookies, {
-  proxy: {  // optional
-    host: process.env.PROXY_HOST,
-    port: process.env.PROXY_PORT,
-    auth: {
-      username: process.env.PROXY_USERNAME,
-      password: process.env.PROXY_PASSWORD
-    },
-    protocol: process.env.PROXY_PROTOCOL
-  }
+  // proxy: {  // optional
+  //   host: process.env.PROXY_HOST,
+  //   port: process.env.PROXY_PORT,
+  //   auth: {
+  //     username: process.env.PROXY_USERNAME,
+  //     password: process.env.PROXY_PASSWORD
+  //   },
+  //   protocol: process.env.PROXY_PROTOCOL
+  // }
 });
 
-async function main() {
-  while (true) {
-    let prompt = await new Promise((resolve) => {
-      rl.question("You: ", (answer) => {
-        resolve(answer);
-      });
-    });
+server.get("/ask", async (req, res) => {
+  try {
+    // const { prompt, conversationId } = req['body'];
+    const prompt = 'What is your name?';
+    let response = await bot.ask(prompt);
+    res.status(200).send(response);
+  } catch(e) {res.status(400).send({})}
+});
 
-    process.stdout.write("Google Bard: ");
-    await bot.askStream(res => {
-      process.stdout.write(res.toString());
-    }, prompt);
-    console.log();
-  }
-}
-
-main();
+let port = 888;
+server.listen(port, () => console.log(`listening to ${port}`));
